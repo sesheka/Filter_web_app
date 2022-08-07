2396ce24489c908ef484d6a9f1cfda00d95692e6
sesheka <89135416+sesheka@users.noreply.github.com>
2022-08-07 10:23:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+}
+function draw()
+{
+    image(video,0,0,300,300)
+}
+function take_snapshot()
+{
+    save('realtime_filter');
+}
